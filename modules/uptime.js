193ba390ec39520e9d0d2d62ea905ad9da1ae881
v801 <32247825+v801@users.noreply.github.com>
2022-07-23 15:53:31 -0600

    more messages!

--- modules/uptime.js
+++ modules/uptime.js
@@ -7,7 +7,15 @@ module.exports = {
         .setDescription('Replies with the bots uptime.'),
     async execute(bot, interaction) {
         const uptime = humanizeDuration(bot.uptime, { round: true });
-        const uptimeMessage = `:clock2: Uptime: ${uptime}`;
+        const uptimeMessagePrefix = `:clock2: ${uptime} `;
+        const uptimeMessageSuffixes = [
+            "since the most recent cord pull.",
+            "SINCE THE GREAT POWER SURGE. :zap: :zap: :zap:",
+            "have elapsed since ^C^C^C^C^C.",
+            "since the last catastrophic failure."
+        ];
+        const randomSuffix = uptimeMessageSuffixes[Math.floor(Math.random()*uptimeMessageSuffixes.length)];
+        const uptimeMessage = uptimeMessagePrefix + randomSuffix;
         return interaction.reply(uptimeMessage);
     },
 };
