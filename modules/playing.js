da88b481845f17b6eaba212023d1449d7736485b
v801 <32247825+v801@users.noreply.github.com>
2022-07-28 03:38:42 -0600

    fix indents

--- modules/playing.js
+++ modules/playing.js
@@ -2,19 +2,19 @@ const { SlashCommandBuilder } = require('discord.js');
 const rgn = require('random-game-name');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('playing')
-		.setDescription('Change what game the bot is playing.'),
-	async execute(bot, interaction) {
+    data: new SlashCommandBuilder()
+        .setName('playing')
+        .setDescription('Change what game the bot is playing.'),
+    async execute(bot, interaction) {
         const errorMessage = 'Unable to set game.';
-		try {
-			const game = rgn();
-			const playingGameMessage = `:video_game: Now Playing: ${game}`;
-			await bot.user.setActivity(game);
-			return interaction.reply(playingGameMessage);
-		} catch (error) {
-			console.log(error);
-			return interaction.reply(errorMessage);
-		}
-	},
+        try {
+            const game = rgn();
+            const playingGameMessage = `:video_game: Now Playing: ${game}`;
+            await bot.user.setActivity(game);
+            return interaction.reply(playingGameMessage);
+        } catch (error) {
+            console.log(error);
+            return interaction.reply(errorMessage);
+        }
+    },
 };
